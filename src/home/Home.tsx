451d837b82cedbf675018ac837a40456b330eb44
Belu06 <beluh065@gmail.com>
2024-08-06 20:06:28 -0300

    Hice otra vez el codigo que no se había guardado (Home de Roomsite)

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,13 +1,37 @@
-import { Container } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Typography,
+} from "@mui/material";
 import MainSection from "@/src/home/components/MainSection";
+import Footer from "../layout/components/Footer";
 
 const Home = () => {
   return (
-    <>
-      <Container maxWidth={false}>
-        <MainSection />
-      </Container>
-    </>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="https://covive.mx/wp-content/uploads/elementor/thumbs/people-gathered-inside-house-sitting-on-sofa-1054974-scaled-1-qph8srorxykw36dvp3o4ow2f7ut0y2n9akdt11y6o0.jpg"
+        title="imagen"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Roomies
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          rhbdtfnjfyjdrhnjth
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Registrarse</Button>
+        <Button size="small">Iniciar Sesion</Button>
+      </CardActions>
+      <Footer></Footer>
+    </Card>
   );
 };
 
--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,8 +1,9 @@
-import { Box } from "@mui/material";
+import { Box, Button, Card, CardActions, Typography } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
-  return <Box component={"footer"} />;
+  return <h2>¿No querés ver esto?</h2>
+  <button>Saltar</button>;
 };
 
 export default Footer;
